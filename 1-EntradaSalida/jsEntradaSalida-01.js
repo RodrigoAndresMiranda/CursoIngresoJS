9b795d08f1609e04b774381745c6276f67b6cffe
Rodrigo Andres Miranda <RodrigoAndresMiranda19@gmail.com>
2020-07-22 11:45:00 -0300

    un mensaje

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,7 +3,10 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Esto funciona de maravilla");
+	
+	alert("Hola cotanza");
+	alert("Etsito");
+
 // comentarios antes o despues de la llave//
 // alert = mensaje literal	
 }
